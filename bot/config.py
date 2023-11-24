020abc5cf9336e552bc56b634bf7511c0a8e1969
Suraj <116282102+suraj0-11@users.noreply.github.com>
2023-11-24 22:16:43 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,12 +12,6 @@ try:
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 
-    Config.APP_ID = 3281305
-    Config.API_HASH = "a9e62ec83fe3c22379e3e19195c8b3f6"
-    Config.BOT_TOKEN = "5723565825:AAHarxi7BDdAkAxQ-NSPcCr9c1qnJLiCSZg"
-    Config.DEV = 5585763218
-    Config.OWNER = 5585763218
-    Config.THUMBNAIL = "https://telegra.ph/file/5d89442e124f10bb5f774.jpg"
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
